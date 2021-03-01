7dd90cb2689ccdd6cab4dfdc5ebaf438dbac4f09
MLuna <lunamrko@gmail.com>
2021-02-28 22:06:14 -0500

    Filtros

---
+++ MiFiltro/MiFiltro/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiFiltro.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize]
+        // GET: Home
+        public string Index()
+        {
+            return "Este es mi controlador Home";
+        }
+        [Authorize (Roles ="Admin")]//Filtro por rol
+        [OutputCache(Duration =10)] //mantiene la cache por 10sg
+        public string HoraActual() {
+            return DateTime.Now.ToString("T");
+        }
+    }
+}
