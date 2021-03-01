6d4c107d2128b76b1bdc47fbae2583921a157e1e
MLuna <lunamrko@gmail.com>
2021-02-28 19:39:08 -0500

    Acciones

---
+++ Mi controlador/Mi controlador/Controllers/ProveedoresController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mi_controlador.Controllers
+{
+    public class ProveedoresController : Controller
+    {
+        // GET: Proveedores
+        public string TodosLosProveedores()
+        {
+            return @"<ul>
+                     <li>Colombina</li>
+                     <li>Super</li>
+                     <li>Aldor</li>
+                     <li>Fini</li>
+                    </ul>";
+        }
+    }
+}
