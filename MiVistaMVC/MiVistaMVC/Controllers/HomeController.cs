b343f9f61b0cfe2f3dd8c6c06aa6d850a908a68a
MLuna <lunamrko@gmail.com>
2021-03-01 20:57:48 -0500

    Vistas

---
+++ MiVistaMVC/MiVistaMVC/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiVistaMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string MiControlador() 
+        {
+            return "Este es mi controlador";
+        }
+        public ActionResult MiVista()
+        {
+            return View();
+        }
+    }
+}
---
+++ MiVistaMVC/MiVistaMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MiVistaMVC.MvcApplication" Language="C#" %>
