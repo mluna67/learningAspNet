5b954e93e1b5c24c669e091cb0eec448a135bcd0
MLuna <lunamrko@gmail.com>
2021-03-02 21:51:48 -0500

    Modelos

---
+++ MiModeloMVC/MiModeloMVC/Controllers/ClientesController.cs
@@ -0,0 +1,112 @@
+﻿using MiModeloMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiModeloMVC.Controllers
+{
+    public class ClientesController : Controller
+    {
+        // GET: Clientes
+        public ActionResult Index()
+        {
+            var Clientes = from e in TodosLosClieentes()
+                           orderby e.ID
+                           select e;
+            return View(Clientes);
+        }
+
+        // GET: Clientes/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Clientes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clientes/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Clientes/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Clientes/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Clientes/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Clientes/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        [NonAction]
+        public List<Clientes> TodosLosClieentes()
+        {
+            return new List<Clientes>
+            {
+                new Clientes{
+                    ID =1,
+                    nombre = "Angel",
+                    FechaAlta = DateTime.Parse(DateTime.Today.ToString()),
+                    edad = 30
+                },
+                new Clientes{
+                    ID =2,
+                    nombre = "Patricia",
+                    FechaAlta = DateTime.Parse(DateTime.Today.ToString()),
+                    edad = 35
+                },
+            };
+        }
+    }
+}
